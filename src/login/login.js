21a5d862d20cf7147dd705d3d61c31d85e26a496
Árlisson Alves <arlisson234529@gmail.com>
2026-02-27 11:39:38 -0300

    Estável, removendo filtro de whatsapp

--- src/login/login.js
+++ src/login/login.js
@@ -79,13 +79,13 @@ document.addEventListener("DOMContentLoaded", async () => {
     let value = e.target.value;
     const isEmail = /[a-zA-Z@]/.test(value);
 
-    if (!isEmail) {
-      value = value.replace(/\D/g, "");
-      if (value.length > 11) value = value.slice(0, 11);
-      if (value.length > 2) value = value.replace(/^(\d{2})(\d)/, "($1) $2");
-      if (value.length > 7) value = value.replace(/(\d)(\d{4})$/, "$1-$2");
-      e.target.value = value;
-    }
+    // if (!isEmail) {
+    //   value = value.replace(/\D/g, "");
+    //   if (value.length > 11) value = value.slice(0, 11);
+    //   if (value.length > 2) value = value.replace(/^(\d{2})(\d)/, "($1) $2");
+    //   if (value.length > 7) value = value.replace(/(\d)(\d{4})$/, "$1-$2");
+    //   e.target.value = value;
+    // }
   });
 
   // --- VALIDAÇÃO EM TEMPO REAL (E-MAIL) ---
