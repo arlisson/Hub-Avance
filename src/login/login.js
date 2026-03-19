1e342571ab0868e2870e89b98677c7251f505146
Breno Luiz <brenooh7@gmail.com>
2026-03-19 10:41:06 -0300

    testando versão antiga do sie

--- src/login/login.js
+++ src/login/login.js
@@ -88,7 +88,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   const { data } = await sb.auth.getSession();
 
   if (data?.session) {
-    window.location.href = "../hub/hub2.html";
+    window.location.href = "../hub/hub.html";
     return;
   }
 
@@ -203,7 +203,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
       if (error) throw error;
 
-      window.location.href = "../hub/hub2.html";
+      window.location.href = "../hub/hub.html";
     } catch (error) {
       alert(`Erro: ${error?.message || "Falha no login."}`);
     } finally {
